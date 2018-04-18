6553731a8bf564fda714950bdafac2f5a48b8e02
ClintonTak <takc@bc.edu>
2018-04-18 11:48:12 -0400

    changed the way the printing works to make later modifications to the output easier, now output is stored in a string to be manipulated later before printing

--- bitmap_editor-master/lib/bitmap_editor.rb
+++ bitmap_editor-master/lib/bitmap_editor.rb
@@ -9,26 +9,27 @@ class BitmapEditor
 			line = line.gsub(/\s+/, "")#removing whitespace for better parsing 
 			#puts line
 			#puts line[0]
+			outputString = ""
 			case line[0]
 			when 'i'
 				width = line[1].to_i
 				while width > 0
-
 					length = line[2].to_i
 					while length > 0
-						print '0'
+						outputString.concat('0')
 						length -= 1
 					end
-					print "\n"
+					outputString.concat("\n")
 					width -=1
 				end
-
+=begin
 			when 's'
 				puts "There is no image"
-=begin
+
 			else
 				puts 'unrecognised command :(' 
 =end
+			print outputString
 			end 
 		end
 	end
