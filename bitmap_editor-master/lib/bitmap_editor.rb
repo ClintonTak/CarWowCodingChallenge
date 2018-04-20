369e71f59729c2f1aa65008898fd7f330c58ff66
Clinton Tak <takc@bc.edu>
2018-04-20 10:58:11 -0400

    added something to test if the bitmap was properly set up prior to actions on it

--- bitmap_editor-master/lib/bitmap_editor.rb
+++ bitmap_editor-master/lib/bitmap_editor.rb
@@ -2,33 +2,39 @@ class BitmapEditor
 
 	def run(file)
 		@outputArray = [] 
+		properSetUpFlag = false; 
 		return puts "please provide correct file" if file.nil? || !File.exists?(file)
 
 		File.open(file).each do |line|
 			if line.include? "-"
-			   puts "negative number detected, please check input array"
+			   puts "negative number detected, please check input array" 
 			   return 0
 			end
 			subline = line.split
-			case subline[0] 
-				when '#', '/' #adds support for comments in the test file, must start with # and be followed by a space 
-					next
-				when 'i', 'I'
-					handleI(subline[2].to_i, subline[1].to_i)
-				when 'l', 'L' 
-						handleL(subline[1].to_i,subline[2].to_i, subline[3])
-				when 'c', 'C'
-					clearArray()
-				when 'v', 'V'
-					handleV(subline[1].to_i,subline[2].to_i, subline[3].to_i, subline[4]) 
-				when 'h', 'H'
-					handleH(subline[1].to_i, subline[2].to_i, subline[3].to_i, subline[4])
-				when 's', 'S'
-					handleS
-				else
-					puts 'unrecognised command :(' 
-
-				
+			if subline[0] == 'i' or subline[0] == 'I'
+				properSetUpFlag = true; #tests to make sure that the grid has been properly set up first 
+			end
+			if properSetUpFlag == true
+				case subline[0] 
+					when '#', '/' #adds support for comments in the test file, must start with # and be followed by a space 
+						next
+					when 'i', 'I'
+						handleI(subline[2].to_i, subline[1].to_i)
+					when 'l', 'L' 
+							handleL(subline[1].to_i,subline[2].to_i, subline[3])
+					when 'c', 'C'
+						clearArray()
+					when 'v', 'V'
+						handleV(subline[1].to_i,subline[2].to_i, subline[3].to_i, subline[4]) 
+					when 'h', 'H'
+						handleH(subline[1].to_i, subline[2].to_i, subline[3].to_i, subline[4])
+					when 's', 'S'
+						handleS
+					else
+						puts 'unrecognised command :(' 
+				end
+			else 
+				next
 			end 
 		end 
 	end
