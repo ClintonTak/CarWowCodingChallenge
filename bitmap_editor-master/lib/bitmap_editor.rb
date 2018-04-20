4ae3a51866c3de05a0c13c4bfd61beee908437d1
Clinton Tak <takc@bc.edu>
2018-04-20 11:45:43 -0400

    separated code out to a new class to make it more manageable and more object oriented

--- bitmap_editor-master/lib/bitmap_editor.rb
+++ bitmap_editor-master/lib/bitmap_editor.rb
@@ -1,7 +1,7 @@
+require_relative 'bitmap_storage_array.rb'
 class BitmapEditor
 
 	def run(file)
-		@outputArray = [] 
 		properSetUpFlag = false; 
 		return puts "please provide correct file" if file.nil? || !File.exists?(file)
 
@@ -19,17 +19,17 @@ class BitmapEditor
 					when '#', '/' #adds support for comments in the test file, must start with # and be followed by a space 
 						next
 					when 'i', 'I'
-						setUpMap(subline[2].to_i, subline[1].to_i)
+						@output = Bitmap_storage_array.new(subline[2].to_i, subline[1].to_i)
 					when 'l', 'L' 
-						drawPoint(subline[1].to_i,subline[2].to_i, subline[3])
+						@output.drawPoint(subline[1].to_i,subline[2].to_i, subline[3])
 					when 'c', 'C'
-						clearArray()
+						@output.clearArray()
 					when 'v', 'V'
-						drawVerticalLine(subline[1].to_i,subline[2].to_i, subline[3].to_i, subline[4]) 
+						@output.drawVerticalLine(subline[1].to_i,subline[2].to_i, subline[3].to_i, subline[4]) 
 					when 'h', 'H'
-						drawHorizontalLine(subline[1].to_i, subline[2].to_i, subline[3].to_i, subline[4])
+						@output.drawHorizontalLine(subline[1].to_i, subline[2].to_i, subline[3].to_i, subline[4])
 					when 's', 'S'
-						displayBitMap
+						@output.displayBitMap
 					else
 						puts 'unrecognised command :(' 
 				end
@@ -38,69 +38,4 @@ class BitmapEditor
 			end 
 		end 
 	end
-
-	def clearArray()
-		@outputArray.each_index do |i|
-				@outputArray[i].each_index do |j|
-					@outputArray[i][j] = "O"
-				end
-			end
-		end
-
-	def drawVerticalLine(startline, to, from, color)
-		if to<from #normal case that y1 is less than y2
-			for x in to-1..from-1
-				@outputArray[x][startline-1] = color
-			end 
-		else 
-			for x in from-1..to-1
-				@outputArray[x][startline-1] = color
-			end
-		end	
-	end
-
-	def drawHorizontalLine(startline, to, from, color)
-		if to<from #normal case that x1 is less than x2
-			for x in to..from-1
-				@outputArray[startline-2][x] = color
-			end 
-		else 
-			for x in from..to-1
-				@outputArray[startline-2][x] = color
-			end
-		end
-	end
-
-	def setUpMap(width, length)
-		i = width
-		while i > 0
-			j = length
-			subArray = []
-			while j > 0
-				subArray.push('O')
-				j -= 1
-			end
-			@outputArray.push(subArray)
-			i -=1
-		end
-	end
-
-	def drawPoint(x, y, color)
-		if x< 1 or y > @outputArray[1].size
-			puts "improper x coordinate on 'L' line "
-			return 0
-		elsif y < 1  or y > @outputArray.size
-			puts "improper y coordinate input on 'L' line"
-			return 0
-		else 
-			@outputArray[y-1][x-1] = color
-		end
-	end 
-
-
-	def displayBitMap()
-		puts @outputArray.map{|x| x.join('')}		
-		puts "\n"
-	end
-
 end
--- bitmap_editor-master/lib/bitmap_storage_array.rb
+++ bitmap_editor-master/lib/bitmap_storage_array.rb
@@ -1,36 +1,81 @@
 class Bitmap_storage_array
 
 	def initialize(width, length)
-		@width = width
-		@length = length
 		@outputArray = []
+		i = width 
+		while i > 0
+			j = length
+			subArray = []
+			while j > 0
+				subArray.push('O')
+				j -= 1
+			end
+			@outputArray.push(subArray)
+			i -=1
+		end
+	end
+
+	def clearArray()
+		@outputArray.each_index do |i|
+			@outputArray[i].each_index do |j|
+				@outputArray[i][j] = "O"
+			end
+		end	
+	end
+
+	def drawVerticalLine(startline, to, from, color)
+		if to<from #normal case that y1 is less than y2
+			for x in to-1..from-1
+				@outputArray[x][startline-1] = color
+			end 
+		else 
+			for x in from-1..to-1
+				@outputArray[x][startline-1] = color
+			end
+		end	
+	end
+
+	def drawHorizontalLine(startline, to, from, color)
+		if to<from #normal case that x1 is less than x2
+			for x in to..from-1
+				@outputArray[startline-2][x] = color
+			end 
+		else 
+			for x in from..to-1
+				@outputArray[startline-2][x] = color
+			end
+		end
+	end
+
+	def setUpMap(width, length)
 		i = width
 		while i > 0
 			j = length
 			subArray = []
 			while j > 0
-				#outputString.concat('0')
 				subArray.push('O')
 				j -= 1
 			end
-			#outputString.concat("\n")
 			@outputArray.push(subArray)
 			i -=1
 		end
-		
 	end
 
-	def push(item)
-		@outputArray.push(item)
-	end
+	def drawPoint(x, y, color)
+		if x< 1 or y > @outputArray[1].size
+			puts "improper x coordinate on 'L' line "
+			return 0
+		elsif y < 1  or y > @outputArray.size
+			puts "improper y coordinate input on 'L' line"
+			return 0
+		else 
+			@outputArray[y-1][x-1] = color
+		end
+	end 
 
-
-	def width
-		@width
-	end
-
-	def length
-		@length
+	def displayBitMap()
+		puts @outputArray.map{|x| x.join('')}		
+		puts "\n"
 	end
 
 end
